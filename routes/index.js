698ff132d2542bdf93e151c12e44b1eb1d047d62
yanhamu <kriskatomas360@gmail.com>
2015-08-23 18:40:05 +0200

    added useless functions

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,19 @@
  * GET home page.
  */
 
+getPeople = function () {
+    return [{ name: 'Tomas', vacation: true }, { name: 'Michal', vacation: false }];
+}
+
+log = function () { console.log("l'cocl"); }
+
 exports.index = function (req, res) {
-    res.render('index', { title: 'Amazing Fika', people: ['Tomas', 'Michal'] });
-};
+    res.render('index', {
+        title: 'Amazing Fika', 
+        people: getPeople(), 
+        fs: {
+            serverGetPairs: log,
+            clientGetPairs: log
+        }
+    });
+}
